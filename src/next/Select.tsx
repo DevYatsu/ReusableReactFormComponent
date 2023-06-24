65c4f697af4b4500744addc7a7d7793e025f7323
Yatsu <yatsu.dev@gmail.com>
2023-06-24 21:01:36 +0200

    little change in a function type

--- src/next/Select.tsx
+++ src/next/Select.tsx
@@ -4,7 +4,7 @@ const Select = ({ name, register, values, disabled }: Select) => (
   <>
     <label
       htmlFor={name}
-      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white uppercase"
     >
       {name
         .split("")
@@ -14,7 +14,7 @@ const Select = ({ name, register, values, disabled }: Select) => (
 
     <select
       {...register(name, { disabled })}
-      className="bg-gray-50 border border-gray-300 text-gray-900 w-full text-center text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      className="bg-gray-50 border border-gray-300 py-1 text-gray-900 w-full text-center text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     >
       {values.map((option) => (
         <option value={option.value} key={option.value}>
