d7c12ed726cc6a9fbb74954e0b69cef81c5d8db4
Yatsu <yatsu.dev@gmail.com>
2023-06-24 19:25:48 +0200

    infrastructure changes

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -3,9 +3,17 @@ import { render } from "@testing-library/react";
 import "jest-canvas-mock";
 
 import Form from "../lib/main";
-import { getGenericFormInputsData } from "../lib/utils";
+import { genSelect, genTextArea } from "../lib/utils";
 
-const data = getGenericFormInputsData("username");
+const data = [
+  genTextArea("comment", "Comment...", {
+    required: "Please enter a comment.",
+  }),
+  genSelect("comment", [
+    { value: "test", displayValue: "test" },
+    { value: "test2", displayValue: "test2" },
+  ]),
+];
 
 describe("Common render", () => {
   it("renders without crashing", () => {
