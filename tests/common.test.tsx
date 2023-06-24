e28045ca1a1f314fe2210bca442f8952fe41cdb6
Yatsu <yatsu.dev@gmail.com>
2023-06-24 17:48:46 +0200

    changes in the tsconfig and package json

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -2,8 +2,8 @@ import { render } from "@testing-library/react";
 
 import "jest-canvas-mock";
 
-import Form from "../dist/cjs/main";
-import { getGenericFormInputsData } from "../dist/cjs/utils";
+import Form from "../lib/main";
+import { getGenericFormInputsData } from "../lib/utils";
 
 const data = getGenericFormInputsData("username");
 
