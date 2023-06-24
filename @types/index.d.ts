909617f4450ae8deb7cce327118bb7928b27d69c
Yatsu <yatsu.dev@gmail.com>
2023-06-24 19:47:59 +0200

    fix of the bug regarding types when initializing data array

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,9 +1,14 @@
 import { FormData, FormProps, FormGoal, extraFormData } from "./form";
-import { Input, FormInput } from "./input";
-import { Select, FormSelect } from "./select";
-import { TextArea, FormTextArea } from "./textarea";
+import {
+  Input,
+  FormInput,
+  InputOptions,
+  ReactHookFormPropertiesRestrictions,
+} from "./input";
+import { Select, FormSelect, SelectValues } from "./select";
+import { TextArea, FormTextArea, TextAreaOptions } from "./textarea";
 
 export { FormData, FormProps, FormGoal, extraFormData };
-export { Input, FormInput };
-export { Select, FormSelect };
-export { TextArea, FormTextArea };
+export { Input, FormInput, InputOptions, ReactHookFormPropertiesRestrictions };
+export { Select, FormSelect, SelectValues };
+export { TextArea, FormTextArea, TextAreaOptions };
