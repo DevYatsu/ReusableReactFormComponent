f87403f649f7fc224890528f9407af3497b55e7b
Yatsu <yatsu.dev@gmail.com>
2023-06-24 03:01:01 +0200

    package generated

---
+++ dist/esm/main/Select.js
@@ -0,0 +1,4 @@
+import { jsx as _jsx, Fragment as _Fragment } from "react/jsx-runtime";
+const Select = ({ name, register, values }) => (_jsx(_Fragment, { children: _jsx("select", Object.assign({}, register(name), { children: values.map((option) => (_jsx("option", { value: option.value, children: option.displayValue }))) })) }));
+export default Select;
+//# sourceMappingURL=Select.js.map
