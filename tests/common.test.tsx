4ad7fc7c037fa97dab1aeb7a1c22f4db6eae9102
Yatsu <yatsu.dev@gmail.com>
2023-06-24 17:15:39 +0200

    modification of the lib infrastructure

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -2,13 +2,13 @@ import { render } from "@testing-library/react";
 
 import "jest-canvas-mock";
 
-import generateData from "../lib/utils";
-import { MainForm } from "../index";
+import Form from "../dist/cjs/main";
+import { getGenericFormInputsData } from "../dist/cjs/utils";
 
-const data = generateData("username");
+const data = getGenericFormInputsData("username");
 
 describe("Common render", () => {
   it("renders without crashing", () => {
-    render(<MainForm data={data} submitURL={"/"} />);
+    render(<Form data={data} submitURL={"/"} />);
   });
 });
