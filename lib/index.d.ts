2469c402cf525e8b3860ad3c1791bf5e012dee37
Yatsu <yatsu.dev@gmail.com>
2023-06-25 22:52:23 +0200

    last bugs fixed

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import "./index.css";
+import Form from "./main";
+import NextForm from "./next";
+import { Input, Select, TextArea } from "./components";
+import { genInput, genSelect, genTextArea, getGenericFormInputsData } from "./utils";
+export { Form, NextForm, Input, Select, TextArea, genInput, genSelect, genTextArea, getGenericFormInputsData, };
